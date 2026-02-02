24bae68ac7729b673718f80e3210692bd4ec35c8
Jean Paul Hernandez <jean.paul.hernandez@topodot.com>
2026-02-02 18:23:36 -0500

    Add TodoList and TodoListItem components in ToDoAppComponent structure

--- src/Problem-3-To-Do-App-Feature/ToDoAppComponent.tsx
+++ src/Problem-3-To-Do-App-Feature/ToDoAppComponent.tsx
@@ -1,3 +1,15 @@
 export function ToDoAppComponent() {
+
+    //pass list of 'Todo' objects into TodoList component for this component to render
+
+    return <TodoList />
+}
+
+
+export function TodoList() {
+    return <></>
+}
+
+export function TodoListItem() {
     return <></>
 }
